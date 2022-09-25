b6bc4534281eaeab272a4bc40d6950905bbd7c89
Julia Wang <juliawang240@gmail.com>
2022-09-24 18:55:23 -0700

    Update the website

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,10 @@
-import React, { Component } from "react";
-
-import Header from "./Header";
-import Footer from "./Footer";
-import About from "../pages/About";
-import Projects from "../pages/Projects";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Header />
-
-        <main>
-          <div className="content">
-            <Switch>
-              <Route exact path="/">
-                <About />
-              </Route>
-              <Route exact path="/projects">
-                <Projects />
-              </Route>
-            </Switch>
-          </div>
-        </main>
-
-        <Footer />
-      </div>
-    </Router>
+    <div className="container">
+      <div className="copyright">©2022 JULIA WANG</div>
+    </div>
   );
 }
 
