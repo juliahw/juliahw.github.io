058b2d042ffca0bd0900f21fadf00f79f485cce5
Julia Wang <julia.wang@mulesoft.com>
2022-09-27 16:21:43 -0700

    Center the copyright text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
               id="path"
               fill="none"
               stroke="white"
-              transform="translate(0, 35) scale(1.75)"
+              transform="translate(15, 35) scale(1.75)"
               d="M1 8C9.33333 -1.33333 17.6667 -1.33333 26 8C34.3333 17.3333 42.6667 17.3333 51 8C59.3333 -1.33333 67.6667 -1.33333 76 8C84.3333 17.3333 92.6667 17.3333 101 8"
             />
           </defs>
