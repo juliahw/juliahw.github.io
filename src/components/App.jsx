05ed9577b5ff7bfa72037873125bb2f228f87487
Julia Wang <julia.wang@mulesoft.com>
2022-09-27 16:34:00 -0700

    Improve the SVG styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,12 +10,14 @@ function App() {
               id="path"
               fill="none"
               stroke="white"
-              transform="translate(15, 35) scale(1.75)"
+              transform="translate(0, 35) scale(2)"
               d="M1 8C9.33333 -1.33333 17.6667 -1.33333 26 8C34.3333 17.3333 42.6667 17.3333 51 8C59.3333 -1.33333 67.6667 -1.33333 76 8C84.3333 17.3333 92.6667 17.3333 101 8"
             />
           </defs>
-          <text x="50%" dominantBaseline="middle" textAnchor="middle">
-            <textPath href="#path">©2022 JULIA WANG</textPath>
+          <text>
+            <textPath href="#path" startOffset="50%" textAnchor="middle">
+              <tspan dy="4">©2022 JULIA WANG</tspan>
+            </textPath>
           </text>
         </svg>
       </div>
